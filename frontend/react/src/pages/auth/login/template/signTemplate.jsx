fa70a6e82b464ff5c4a287311df098f6a6de19fe
kimyoungsik <kimyoungsik7@naver.com>
2023-01-18 17:54:02 +0900

    design: 로그인 화면 구성1 로그인 화면의 큰 틀과 버튼 구성 완료 세부작업 필요 closes #S08P12A810-68

--- frontend/react/src/pages/auth/login/template/signTemplate.jsx
+++ frontend/react/src/pages/auth/login/template/signTemplate.jsx
@@ -7,24 +7,27 @@ import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import Switch from "@mui/material/Switch";
 // import Grid from "@mui/material/Grid";
-// import MuiLink from "@mui/material/Link";
+import MuiLink from "@mui/material/Link";
 
 // @mui icons
 // import FacebookIcon from "@mui/icons-material/Facebook";
-// import GitHubIcon from "@mui/icons-material/GitHub";
+import GitHubIcon from "@mui/icons-material/GitHub";
 // import GoogleIcon from "@mui/icons-material/Google";
 
 // Material Dashboard 2 React components
 import MDBox from "components/MDBox";
 import MDTypography from "components/MDTypography";
 import MDInput from "components/MDInput";
-import MDButton from "components/MDButton";
+// import MDButton from "components/MDButton";
+import { Button } from "@mui/material";
+import "./signTemplate.css";
 
 // Authentication layout components
-import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+// import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
 
 // Images
 // import bgImage from "assets/images/bg-sign-in-basic.jpeg";
+// import CardImg from "assets/images/bundler/bundlerRabbit.png";
 
 function SignTemplate() {
   const [rememberMe, setRememberMe] = useState(false);
@@ -32,8 +35,8 @@ function SignTemplate() {
   const handleSetRememberMe = () => setRememberMe(!rememberMe);
 
   return (
-    <DashboardLayout>
-      <Card>
+    <div className="container">
+      <Card sx={{ minWidth: 500, maxWidth: 600 }}>
         {/* 이메일 비밀번호 받기 */}
         <MDBox pt={4} pb={3} px={3}>
           <MDBox component="form" role="form">
@@ -57,30 +60,58 @@ function SignTemplate() {
             </MDBox>
             {/* 로그인 버튼 */}
             <MDBox mt={4} mb={1}>
-              <MDButton variant="gradient" color="info" fullWidth>
-                sign in
-              </MDButton>
+              <Button
+                sx={{
+                  bgcolor: "#81D8CF",
+                  color: "#000000",
+                  fontSize: "midium",
+                  fontWeight: "bold",
+                }}
+                variant="contained"
+                fullWidth
+              >
+                로그인
+              </Button>
             </MDBox>
             {/* 회원가입 버튼 */}
-            <MDBox mt={3} mb={1} textAlign="center">
-              <MDTypography variant="button" color="text">
-                Don&apos;t have an account?{" "}
-                <MDTypography
-                  component={Link}
-                  to="/authentication/sign-up"
-                  variant="button"
-                  color="info"
-                  fontWeight="medium"
-                  textGradient
+            <MDBox mt={0} mb={1} textAlign="center">
+              <MDBox mt={4} mb={1} component={Link} to="/authentication/sign-up">
+                <Button
+                  sx={{
+                    bgcolor: "#FFFFFF",
+                    color: "#000000",
+                    fontSize: "midium",
+                    fontWeight: "bold",
+                  }}
+                  variant="contained"
+                  fullWidth
                 >
-                  Sign up
-                </MDTypography>
-              </MDTypography>
+                  회원가입
+                </Button>
+              </MDBox>
+            </MDBox>
+            {/* github 로그인 */}
+            <MDBox mt={0} mb={1} textAlign="center">
+              <MDBox mt={4} mb={1} component={MuiLink} to="">
+                <Button
+                  sx={{
+                    bgcolor: "#FFFFFF",
+                    color: "#000000",
+                    fontSize: "midium",
+                    fontWeight: "bold",
+                  }}
+                  variant="contained"
+                  fullWidth
+                  startIcon={<GitHubIcon />}
+                >
+                  GitHub
+                </Button>
+              </MDBox>
             </MDBox>
           </MDBox>
         </MDBox>
       </Card>
-    </DashboardLayout>
+    </div>
   );
 }
 
