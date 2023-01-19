cbe4ad76b0ef6ec4a7f69bffa70f351b4ae4daae
kimyoungsik <kimyoungsik7@naver.com>
2023-01-19 17:41:56 +0900

    feat: 로그아웃 구현 및 서버에서 토큰 받기 성공 로그인 시  로그인 성공화면과 로그아웃 버튼이 나타나고 로그아웃 버튼을 누르면 로그아웃 구현 성공 로그인시 유저 정보를 알맞게 보내면 서버에서 토큰을 받아옴 closes #S08P12A810-81, #S08P12A810-86, #S08P12A810-89

--- frontend/react/src/pages/auth/login/template/components/Login.jsx
+++ frontend/react/src/pages/auth/login/template/components/Login.jsx
@@ -1,28 +1,34 @@
 import { React, useState } from "react";
 import "./login.css";
 import axios from "axios";
-
+// 로그인 함수를 실행해서
 function Login() {
+  // setEmail 함수로 email 대응 하는 값을 변경할 수 있게 useState 생성
   const [email, setEmail] = useState("");
+  // setPassword 함수로 password 대응 하는 값을 변경할 수 있게 useState 생성
   const [password, setPassword] = useState("");
-
+  // login 함수를 실행하면
   const login = () => {
+    // 아래와 같은 조건으로 axios 보냄
     axios({
       url: "http://localhost:8123/login",
       method: "POST",
       withCredentials: true,
       data: { email, password },
-    }).then((result) => {
-      if (result.status === 200) {
-        window.open("/auth/login", "_self");
-      }
-    });
+    })
+      // axios 요청이 성공한다면 200과 함께 로그인 화면을 보여줌
+      .then((result) => {
+        if (result.status === 200) {
+          window.open("/auth/login", "_self");
+        }
+      });
   };
 
   return (
     <div>
       <div className="loginContainer">
         <div className="inputGroup">
+          {/* 이메일을 받기 위한 input */}
           <label className="inputLabel" htmlFor="emailLogin">
             email
             <input
@@ -35,6 +41,7 @@ function Login() {
           </label>
         </div>
         <div className="inputGroup">
+          {/* password를 받기 위한 input */}
           <label className="inputLabel" htmlFor="passwordLogin">
             password
             <input
@@ -46,6 +53,7 @@ function Login() {
             />
           </label>
         </div>
+        {/* input 받은 정보를 보내는 버튼 / login 함수를 실행시켜 axios 요청으로 전송 */}
         <button type="button" onClick={login} className="loginButton">
           Login
         </button>
