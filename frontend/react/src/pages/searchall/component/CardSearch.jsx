3291668e2ca90cb18e82e39471761d8a54e4a506
kimyoungsik <kimyoungsik7@naver.com>
2023-02-02 17:45:38 +0900

    feat: 피드 검색 카테고리 구현 피드 타입 결정 후 쿼리로 보냄 _ 카드일 때만 세부 선택 구현 카드 타입 결정 후 쿼리로 보냄 카테고리 결정 후 쿼리로 보냄_ 대분류,  중분류에 따라 pk값으로 전송 검색어 피드 타입에 관계 없이 쿼리로 보냄 closes #S0812A810-159 #S08P12A810-161

--- frontend/react/src/pages/searchall/component/CardSearch.jsx
+++ frontend/react/src/pages/searchall/component/CardSearch.jsx
@@ -1,73 +1,140 @@
 import { React, useState } from "react";
 import axios from "axios";
 
-const PRODUCT_DATA = [
+const FeedType = [
   { id: null, value: "카드 + 번들" },
-  { id: "feed_card", value: "카드" },
-  { id: "feed_bundle", value: "번들" },
+  { id: "CARD", value: "카드" },
+  { id: "BUNDLE", value: "번들" },
+];
+
+const CardType = [
+  { id: null, value: "모두검색" },
+  { id: "CARD_PROBLEM", value: "문제" },
+  { id: "CARD_GENERAL", value: "일반" },
+  { id: "CARD_LINK", value: "링크" },
+];
+
+const CategoryIdBig = [
+  { id: null, value: "모두검색" },
+  { id: "1", value: "알고리즘" },
+  { id: "2", value: "CS" },
+  { id: "4", value: "직무" },
+  { id: "5", value: "언어" },
+  { id: "3", value: "기타" },
+];
+
+const CategoryIdSmall1 = [
+  { id: "1", value: "대분류 모두검색" },
+  { id: "6", value: "그래프탐색 / 트리 / 힙" },
+  { id: "7", value: "스택 / 큐 / 정렬" },
+  { id: "8", value: "완전탐색 / 이분탐색" },
+  { id: "9", value: "탐욕 / 동적계획법 / 해시" },
+  { id: "10", value: "기타 (해시 .. 등)" },
+];
+const CategoryIdSmall2 = [
+  { id: "2", value: "대분류 모두검색" },
+  { id: "11", value: "수학" },
+  { id: "12", value: "컴퓨터구조" },
+  { id: "13", value: "운영체제" },
+  { id: "14", value: "자료구조" },
+  { id: "15", value: "네트워크" },
+  { id: "16", value: "데이터베이스" },
+  { id: "17", value: "기타" },
+];
+const CategoryIdSmall3 = [
+  { id: "4", value: "대분류 모두검색" },
+  { id: "22", value: "면접" },
+  { id: "23", value: "백엔드" },
+  { id: "24", value: "프론트엔드" },
+  { id: "25", value: "인프라" },
+  { id: "26", value: "데이터베이스 엔지니어" },
+  { id: "27", value: "기타" },
+];
+const CategoryIdSmall4 = [
+  { id: "5", value: "대분류 모두검색" },
+  { id: "28", value: "C / C++" },
+  { id: "29", value: "JAVA" },
+  { id: "30", value: "Python" },
+  { id: "31", value: "Kotlin" },
+  { id: "32", value: "JS / HTML / CSS" },
+  { id: "33", value: "SQL" },
+  { id: "34", value: "기타" },
+];
+const CategoryIdSmall5 = [
+  { id: "3", value: "대분류 모두검색" },
+  { id: "18", value: "기업분석" },
+  { id: "19", value: "IT기사 / 트렌드" },
+  { id: "21", value: "자격증" },
+  { id: "20", value: "기타" },
 ];
 
 function CardSearch() {
   // input에 검색어를 입력하면 setSearch 사용하여 search 변경
   const [search, setSearch] = useState("");
-  // 검색어를 요청을 보내 받은 정보를 setResultId를 사용하여 resultId를 리스트 안에 저장
-  // const [resultId, setResultId] = useState([]);
+  // 피드타입 선택
+  const [feedType, setFeedType] = useState("");
+  const feedTypeDropbox = (e) => {
+    const { value } = e.target;
+    setFeedType(FeedType.filter((el) => el.value === value)[0].id);
+  };
+
+  // 카드타입 선택
+  const [cardType, setCardType] = useState("");
+  const cardTypeDropbox = (e) => {
+    const { value } = e.target;
+    setCardType(CardType.filter((el) => el.value === value)[0].id);
+  };
 
   // 카테고리 선택
-  const [selectFeed, setSelectedFeed] = useState("");
-  // onChange 이벤트가 발생한 target을 받아와 value값이 할당해준다.
-  const feedDropbox = (e) => {
+  const [categoryId, setCategoryId] = useState("");
+  const [categoryId2, setcategoryId2] = useState("");
+
+  // 대분류
+  const categoryIdBigDropbox = (e) => {
     const { value } = e.target;
-    // 상품코드에 넣을 데이터
-    setSelectedFeed(PRODUCT_DATA.filter((el) => el.value === value)[0].id);
+    setCategoryId(CategoryIdBig.filter((el) => el.value === value)[0].id);
+    setcategoryId2(CategoryIdBig.filter((el) => el.value === value)[0].id);
+  };
+  // 중분류 1
+  const categoryIdSmall1Dropbox = (e) => {
+    const { value } = e.target;
+    setCategoryId(CategoryIdSmall1.filter((el) => el.value === value)[0].id);
+  };
+  // 중분류 2
+  const categoryIdSmall2Dropbox = (e) => {
+    const { value } = e.target;
+    setCategoryId(CategoryIdSmall2.filter((el) => el.value === value)[0].id);
+  };
+  // 중분류 3
+  const categoryIdSmall3Dropbox = (e) => {
+    const { value } = e.target;
+    setCategoryId(CategoryIdSmall3.filter((el) => el.value === value)[0].id);
+  };
+  // 중분류 4
+  const categoryIdSmall4Dropbox = (e) => {
+    const { value } = e.target;
+    setCategoryId(CategoryIdSmall4.filter((el) => el.value === value)[0].id);
+  };
+  // 중분류 5
+  const categoryIdSmall5Dropbox = (e) => {
+    const { value } = e.target;
+    setCategoryId(CategoryIdSmall5.filter((el) => el.value === value)[0].id);
   };
 
   // clickSearchBtn 실행시켜 회원검색 진행
   const clickSearchBtn = () => {
-    if (selectFeed === "feed_card") {
-      console.log("0000000000000000");
-      axios({
-        url: "http://localhost:8080/api/v4/feeds/cards",
-        method: "get",
-        params: { search },
-        withCredentials: true,
-      }).then((result) => {
-        if (result.data) {
-          console.log(result.data);
-          // const resultId = result.data.userInfo;
-          // setResultId(result.data.userInfo);
-        }
-      });
-    } else if (selectFeed === "feed_bundle") {
-      console.log("1111111111111111");
-      axios({
-        url: "http://localhost:8080/api/v1/feeds/bundles",
-        method: "get",
-        params: { search },
-        withCredentials: true,
-      }).then((result) => {
-        if (result.data) {
-          console.log(result.data);
-
-          // const resultId = result.data.userInfo;
-          // setResultId(result.data.userInfo);
-        }
-      });
-    } else {
-      console.log("222222222222222");
-      axios({
-        url: "http://localhost:8080/api/v4/feeds/cards",
-        method: "get",
-        params: { search },
-        withCredentials: true,
-      }).then((result) => {
-        if (result.data) {
-          console.log(result.data);
-          // const resultId = result.data.userInfo;
-          // setResultId(result.data.userInfo);
-        }
-      });
-    }
+    axios({
+      url: "http://localhost:8080/api/v4/feeds/",
+      method: "get",
+      params: { feedType, cardType, categoryId, search },
+      withCredentials: true,
+    }).then((result) => {
+      if (result.data) {
+        console.log(result.data);
+        // const resultId = result.data.userInfo;
+        // setResultId(result.data.userInfo);
+      }
+    });
   };
 
   return (
@@ -118,16 +185,112 @@ function CardSearch() {
           alignItems: "center",
         }}
       >
-        {/* 카테고리 */}
-        <select onChange={feedDropbox}>
-          {PRODUCT_DATA.map((el) => (
+        {/* 피드타입 */}
+        <select onChange={feedTypeDropbox}>
+          {FeedType.map((el) => (
             <option key={el.id}>{el.value}</option>
           ))}
         </select>
-        {/* 카테고리별 저장값 */}
+        {/* 피드타입별 저장값 */}
         <div>
-          <p>{selectFeed}</p>
+          <p>{feedType}</p>
         </div>
+
+        {feedType === "CARD" ? (
+          <>
+            {/* 카드타입 */}
+            <select onChange={cardTypeDropbox}>
+              {CardType.map((el) => (
+                <option key={el.id}>{el.value}</option>
+              ))}
+            </select>
+            {/* 카드타입별 저장값 */}
+            <div>
+              <p>{cardType}</p>
+            </div>
+
+            {/* 카테고리 대 */}
+            <select onChange={categoryIdBigDropbox}>
+              {CategoryIdBig.map((el) => (
+                <option key={el.id}>{el.value}</option>
+              ))}
+            </select>
+            {/* 카테고리별 저장값 */}
+            <div>
+              <p>{categoryId2}</p>
+            </div>
+            {/*  eslint-disable-next-line */}
+            {categoryId2 === "1" ? (
+              <>
+                {/* 카테고리 중1 */}
+                <select onChange={categoryIdSmall1Dropbox}>
+                  {CategoryIdSmall1.map((el) => (
+                    <option key={el.id}>{el.value}</option>
+                  ))}
+                </select>
+                {/* 카테고리별 저장값1 */}
+                <div>
+                  <p>{categoryId}</p>
+                </div>
+              </>
+            ) : // eslint-disable-next-line
+            categoryId2 === "2" ? (
+              <>
+                {/* 카테고리 중2 */}
+                <select onChange={categoryIdSmall2Dropbox}>
+                  {CategoryIdSmall2.map((el) => (
+                    <option key={el.id}>{el.value}</option>
+                  ))}
+                </select>
+                {/* 카테고리별 저장값2 */}
+                <div>
+                  <p>{categoryId}</p>
+                </div>
+              </>
+            ) : // eslint-disable-next-line
+            categoryId2 === "4" ? (
+              <>
+                {/* 카테고리 중4 */}
+                <select onChange={categoryIdSmall3Dropbox}>
+                  {CategoryIdSmall3.map((el) => (
+                    <option key={el.id}>{el.value}</option>
+                  ))}
+                </select>
+                {/* 카테고리별 저장값4 */}
+                <div>
+                  <p>{categoryId}</p>
+                </div>
+              </>
+            ) : // eslint-disable-next-line
+            categoryId2 === "5" ? (
+              <>
+                {/* 카테고리 중5 */}
+                <select onChange={categoryIdSmall4Dropbox}>
+                  {CategoryIdSmall4.map((el) => (
+                    <option key={el.id}>{el.value}</option>
+                  ))}
+                </select>
+                {/* 카테고리별 저장값5 */}
+                <div>
+                  <p>{categoryId}</p>
+                </div>
+              </>
+            ) : categoryId2 === "3" ? (
+              <>
+                {/* 카테고리 중3 */}
+                <select onChange={categoryIdSmall5Dropbox}>
+                  {CategoryIdSmall5.map((el) => (
+                    <option key={el.id}>{el.value}</option>
+                  ))}
+                </select>
+                {/* 카테고리별 저장값3 */}
+                <div>
+                  <p>{categoryId}</p>
+                </div>
+              </>
+            ) : null}
+          </>
+        ) : null}
       </div>
     </div>
   );
