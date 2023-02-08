b3d178aae79a996cbaa10b8873950ccfda68bbd4
kimyoungsik <kimyoungsik7@naver.com>
2023-02-08 16:25:20 +0900

    design: 회원 검색 페이지 기능과 디자인 모두 완료 회원 검색 페이지 모두 완료 / 검색한 회원으로 이동하는 동작만 추후 back과 연결하여 확인하면 마무리 closes #S08P12A810-255

--- frontend/react/src/pages/searchid/component/IdSearch.jsx
+++ frontend/react/src/pages/searchid/component/IdSearch.jsx
@@ -1,57 +1,76 @@
 import { React, useState, useEffect } from "react";
 import axios from "axios";
+// 유저정보 이미지가 없을 때
 import defaultimg from "assets/images/bundler/bundler_rabbit_2.png";
-import inputimg from "assets/images/bundler/input.png";
-
+// 유저정보 박스를 재사용하기 위해 불러온 component
 import { Userbox } from "./userbox";
 
+// 유저 정보 검색
 function IdSearch() {
-  const [SearchItem, setSearchItem] = useState("");
+  // 유저 정보 검색을 하기 위해 입력한 검색어 저장
+  const [keyword, setKeyword] = useState("");
+  // 유저 정보 검색 후 결과 값을 리스트로 저장
   const [resultId, setResultId] = useState([]);
-  // searchId를 실행시켜 회원검색 진행
-  const searchId = () => {
-    axios({
-      url: "http://localhost:8123/searchId",
+  // 유저 정보가 없을 때 검색 결과 없음을 저장
+  const [alert, setAlert] = useState("");
+
+  // 유저정보 검색을 위한 요청
+  const searchId = async () => {
+    // get 요청으로 검색어를 쿼리에 담아 요청
+    await axios({
+      // url: "http://localhost:8123/searchId",
+      url: "http://localhost:8087/api/v1/users",
       method: "get",
-      params: { SearchItem },
+      params: { keyword },
       withCredentials: true,
-    }).then((result) => {
-      if (result.data) {
-        console.log(result);
-        console.log(result.data.userInfo);
-        setResultId(result.data.userInfo);
-      }
-    });
+    })
+      // 검색 기록이 있다면 검색 결과를 받아 resultId에 리스트로 저장하고 alert 비움
+      .then((result) => {
+        console.log("검색 완료");
+        setResultId(result.data);
+        setAlert("");
+      })
+      // 검색 기록이 없다면 alert에 검색 결과 없다는 정보를 저장하고 resultId 리스트 비워줌
+      .catch((error) => {
+        console.log("검색 실패");
+        setAlert(error.response.data.message);
+        setResultId([]);
+      });
   };
+
+  // 검색한 유저의 프로필로 이동하기 위한 요청
   const goProfile = (userId) => {
-    console.log(userId);
+    // get 요청으로 url에 유저 아이디를 담아 요청
     axios({
-      url: `http://localhost:8123/api/v1/users/${userId}`,
+      url: `http://localhost:8087/api/v1/users/${userId}/mypage`,
       method: "get",
       withCredentials: true,
-    }).then((result) => {
-      if (result.data) {
-        console.log(result.data);
-        window.open("/profile", "_self");
-      }
-    });
+    })
+      // 유저 정보 이동이 성공했다면 profile 창으로 이동
+      .then((result) => {
+        if (result.status === 200) {
+          window.open("/profile", "_self");
+        }
+      });
   };
-  // input 창에 입력값이 있을 때 onchange로 실해되는 함수
+
+  // 검색창(input 태그)에 입력값이 들어올 때마다 onchange로 실행되는 함수
   const searchHandler = (e) => {
     e.preventDefault();
-    console.log("s", e.target.value);
-    // SearchItem값을 e.target.value로 바꿔줌
-    setSearchItem(e.target.value);
+    // 입력된 값을 keyword에 저장해줌
+    setKeyword(e.target.value);
   };
-  // SearchItem 값이 변경이 생길 때 실행되는 함수
+
+  // keyword 값이 변경이 생길 때마다 실행되는 함수
   useEffect(() => {
-    // console.log(SearchItem);
     // searchId를 실행시켜 회원검색 진행
     searchId();
-  }, [SearchItem]);
+  }, [keyword]);
 
+  // 유저 검색창 랜더링
   return (
     <div>
+      {/* 가운데 정렬을 위한 상위 박스 */}
       <div
         style={{
           display: "flex",
@@ -59,10 +78,11 @@ function IdSearch() {
           alignItems: "center",
         }}
       >
+        {/* 검색바 */}
         <input
-          placeholder="search"
+          placeholder="닉네임을 입력하세요"
           style={{
-            backgroundImage: { inputimg },
+            backgroundSize: "cover",
             backgroundColor: "rgba(0, 0, 0, 0.2)",
             padding: "16px",
             paddingLeft: "30px",
@@ -72,43 +92,72 @@ function IdSearch() {
             margin: "15px",
             width: "70%",
             color: "white",
-            fontSize: "18px",
+            fontSize: "20px",
           }}
           onChange={searchHandler}
         />
       </div>
-      {resultId.map((item, i) => (
+      {/* 검색결과가 없을 때 나오는 알림창 */}
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          paddingTop: "2%",
+        }}
+      >
+        <h1
+          style={{
+            color: "#FFFFFF",
+          }}
+        >
+          {alert}
+        </h1>
+      </div>
+      {/* 검색 결과를 받아 리스트에 담긴 유저정보를 반복해서 보여줌 */}
+      {resultId.map((item) => (
+        // 유저 정보 박스를 가운데 정렬하기 위한 상위 박스
         <div
-          // eslint-disable-next-line
-          key={i}
           style={{
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
           }}
         >
-          <Userbox onClick={() => goProfile(item.id)}>
-            <div
-              style={{
-                paddingTop: "15px",
-                paddingBottom: "15px",
-                width: "5rem",
-                backgroundColor: "white",
-                borderRadius: "100px",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              {item.thumbnailUrl ? (
-                <img
-                  src={item.thumbnailUrl}
-                  alt=""
-                  style={{
-                    width: "80%",
-                  }}
-                />
-              ) : (
+          {/* 유저정보를 담아 보여주기 위한 component / 클릭시 해당 유저 프로필로 이동 */}
+          <Userbox onClick={() => goProfile(item.userId)}>
+            {/* 유저 이미지가 있을 때 */}
+            {item.userProfileImageUrl ? (
+              <img
+                src={item.userProfileImageUrl}
+                alt=""
+                style={{
+                  paddingTop: "0px",
+                  paddingBottom: "0px",
+                  backgroundColor: "white",
+                  borderRadius: "50px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "80px",
+                  height: "80px",
+                }}
+              />
+            ) : (
+              // 유저 이미지가 없을 때
+              <div
+                style={{
+                  paddingTop: "15px",
+                  paddingBottom: "15px",
+                  width: "5rem",
+                  backgroundColor: "white",
+                  borderRadius: "100px",
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                {/* 유저 이미지가 없을 때 기본이미지 */}
                 <img
                   src={defaultimg}
                   alt=""
@@ -116,15 +165,18 @@ function IdSearch() {
                     width: "80%",
                   }}
                 />
-              )}
-            </div>
+              </div>
+            )}
+            {/* 사진 이외의 추가 정보 */}
             <div
               style={{
                 paddingLeft: "20px",
               }}
             >
-              <h3>{item.username}</h3>
-              <p>{item.email}</p>
+              {/* 유저 닉네임 */}
+              <h3>{item.userNickname}</h3>
+              {/* 유저 소개 */}
+              <p>{item.userIntroduction}</p>
             </div>
           </Userbox>
         </div>
