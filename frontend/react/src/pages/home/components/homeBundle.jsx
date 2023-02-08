eb8d711f4c12ac97a6f633ff83fb5cdc479418f4
정세권 <jsk33a@gmail.com>
2023-02-08 13:24:40 +0900

    refactor: Card Bundle Props data 리팩토링 완료
    - props를 객체로 내려보내기 완료
    
    Closes #S08P12A810-211

--- frontend/react/src/pages/home/components/homeBundle.jsx
+++ frontend/react/src/pages/home/components/homeBundle.jsx
@@ -26,9 +26,9 @@ function HomeBundle({ bundleTitle, bundleAuthor, cardList }) {
   const handleCardClose = () => setCardListModal(false);
 
   return (
-    <Card sx={{ m: 2, mt: 3, maxWidth: 800, minHeight: 150, maxHeight: 400 }}>
+    <Card sx={{ ml: 2, mb: 3, maxWidth: 800, minHeight: 200, maxHeight: 400 }}>
       <ModalCardList open={cardListModal} cardList={cardList} handleCardClose={handleCardClose} />
-      <MDBox mx={3} mt={2}>
+      <MDBox mx={3} sx={{ position: "realative" }}>
         <MDBox display="flex" sx={{ flexWrap: "wrap", justifyContent: "space-between" }}>
           <MDBox display="flex" sx={{ alignItems: "center", width: "80%" }}>
             <MDBox
@@ -43,7 +43,7 @@ function HomeBundle({ bundleTitle, bundleAuthor, cardList }) {
             />
             <MDBox mx={2} width="70%">
               <MDTypography variant="h4" textTransform="capitalize" fontWeight="bold">
-                [번들]&nbsp;{bundleTitle}
+                [번들]
               </MDTypography>
               <MDTypography variant="overline" mt={1}>
                 {bundleAuthor}
@@ -55,9 +55,19 @@ function HomeBundle({ bundleTitle, bundleAuthor, cardList }) {
             <ScrapButton />
           </MDBox>
         </MDBox>
-        <MDBox>
-          {cardList !== null && <Button onClick={handleCardListOpen}>번들 상세보기</Button>}
+        <MDBox mt={2} mb={3}>
+          <MDTypography display="inline" variant="h6" textTransform="capitalize" fontWeight="bold">
+            {bundleTitle}
+          </MDTypography>
         </MDBox>
+        <MDBox mt={2} mb={3}>
+          <MDTypography variant="body2" component="p" color="text">
+            {bundleTitle}
+          </MDTypography>
+        </MDBox>
+        {/* <MDBox sx={{ position: "absolute", bottom: "0", mb: "1" }}> */}
+        {cardList !== null && <Button onClick={handleCardListOpen}>번들 상세보기</Button>}
+        {/* </MDBox> */}
       </MDBox>
     </Card>
   );
