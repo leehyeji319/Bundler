1f727cffdf2cf3a04df4ab9fdc73340c62a7ce56
안태윤 <cfdw011@gmail.com>
2023-02-13 17:22:04 +0900

    refactor(Area*.java): Change mypage api response type
    Change input, output type of api
    Closes #S08P12A810-319

--- backend/bundler/src/main/java/com/ssafy/bundler/controller/AreaController.java
+++ backend/bundler/src/main/java/com/ssafy/bundler/controller/AreaController.java
@@ -1,5 +1,10 @@
 package com.ssafy.bundler.controller;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.bundler.domain.Area;
+import com.ssafy.bundler.dto.area.AreaDto;
 import com.ssafy.bundler.service.AreaService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,12 +26,34 @@ public class AreaController {
 	private final AreaService areaService;
 	@GetMapping(value = "/{userId}")
 	public ResponseEntity<?> get(@PathVariable("userId") Long userId){
-		Area area =  areaService.getArea(userId);
+		Area userArea =  areaService.getArea(userId);
+		Object[] job = getArrayFromString(userArea.getJob());
+		Object[] skill = getArrayFromString(userArea.getSkill());
+		AreaDto area = new AreaDto(userArea.getUserId(),job,skill);
+
 		return ResponseEntity.ok(area);
 	}
 	@PostMapping
-	public ResponseEntity<?> post(@RequestBody Area area){
-		areaService.create(area);
-		return ResponseEntity.ok("created");
+	public ResponseEntity<?> post(@RequestBody AreaDto area){
+		Area newArea  = Area.builder()
+			.userId(area.getUserId())
+			.job(Arrays.toString(area.getJob()))
+			.skill(Arrays.toString(area.getSkill()))
+			.build();
+		areaService.create(newArea);
+		return ResponseEntity.ok("create");
+	}
+
+	Object[] getArrayFromString(String str){
+		List<String> arr = new ArrayList<>();
+
+		String plain = str.substring(1,str.length()-1);
+		StringTokenizer st = new StringTokenizer(plain,", ");
+
+		while(st.hasMoreTokens()){
+			arr.add(st.nextToken());
+		}
+
+		return arr.toArray();
 	}
 }
---
+++ backend/bundler/src/main/java/com/ssafy/bundler/dto/area/AreaDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.bundler.dto.area;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Builder
+public class AreaDto {
+
+	private Long userId;
+	private Object[] job;
+	private Object[] skill;
+
+}
